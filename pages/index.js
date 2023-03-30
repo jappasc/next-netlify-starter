f197b11ffb826bf4de8c211b767e7ffe51294ce0
José Antonio Pascual Gómez <joseantonio.pascual@elca.ch>
2023-03-30 10:56:33 +0200

    another test

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
-        <script type="text/javascript">{alert("Pringaoooooooo")}</script>
+        <script type="text/javascript">{window.alert("Pringaoooooooo")}</script>
       </Head>
 
       <main>
